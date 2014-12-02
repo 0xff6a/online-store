0c1ea40d4bc10864fc968423313cb4bf25a7fe72
Jeremy Fox <foxjerem@gmail.com>
2014-12-02 14:34:04 +0000

    added http load for products

--- app/js/app.js
+++ app/js/app.js
@@ -2,110 +2,20 @@
 
   var app = angular.module("online-store", []);
 
-  app.controller("ProductController", function($scope) {
-    $scope.products = all;
-  });
+  app.controller("ProductController", ["$scope", "$http", function($scope, $http) {
+    $scope.products = [];
 
-  app.controller("VoucherController", function($scope) {
-    $scope.vouchers = all_v;
-  });
+    $http.get("data/products.json").success(function(data) {
+      $scope.products = data;
+    });
+  }]);
 
-  var all_v = 
-  [
-    { 
-      "discount": 5,
-      "conditions": []
-    },
-    { 
-      "discount": 10,
-      "conditions": ["total > 50"]
-    },
-    { 
-      "discount": 15,
-      "conditions":["total > 75", "hasFootwearItem"]
-    }
-  ];
+  app.controller("VoucherController", ["$scope", "$http", function($scope, $http) {
+    $scope.vouchers = [];
 
-  var all = 
-  [
-    {
-      "name": "Almond Toe Court Shoes, Patent Black",
-      "category": "Women's Footwear",
-      "price": 99.00,
-      "stock": 5
-    },
-    {
-      "name": "Suede Shoes, Blue",
-      "category": "Women's Footwear",
-      "price": 42.00,
-      "stock": 4
-    },
-    {
-      "name": "Leather Driver Saddle Loafers, Tan",
-      "category": "Men's Footwear",
-      "price": 34.00,
-      "stock": 12
-    },
-    {
-      "name": "Flip Flops, Red",
-      "category": "Men's Footwear",
-      "price": 19.00,
-      "stock": 6
-    },
-    {
-      "name": "Flip Flops, Blue",
-      "category": "Men's Footwear",
-      "price": 19.00,
-      "stock": 0
-    },
-    {
-      "name": "Gold Button Cardigan, Black",
-      "category": "Women's Casualwear",
-      "price": 167.00,
-      "stock": 6
-    },
-    {
-      "name": "Cotton Shorts, Medium Red",
-      "category": "Women's Casualwear",
-      "price": 30.00,
-      "stock": 5
-    },
-    {
-      "name": "Fine Stripe Short Sleeve Shirt, Grey",
-      "category": "Men's Casualwear",
-      "price": 49.99,
-      "stock": 9
-    },
-    {
-      "name": "Fine Stripe Short Sleeve Shirt, Green",
-      "category": "Men's Casualwear",
-      "price": 39.99,
-      "stock": 3
-    },
-    {
-      "name": "Sharksin Waistcoat, Charcoal",
-      "category": "Men's Formalwear",
-      "price": 75.99,
-      "stock": 2
-    },
-      {
-      "name": "Light Patch Pocket Blazer, Deer",
-      "category": "Men's Casualwear",
-      "price": 175.00,
-      "stock": 1
-    },
-    {
-      "name": "Bird Print Dress, Black",
-      "category": "Women's Formalwear",
-      "price": 270.00,
-      "stock": 10
-    },
-    {
-      "name": "Mid Twist Cut-Out Dress, Pink",
-      "category": "Women's Formalwear",
-      "price": 540.00,
-      "stock": 5
-    }
-  ];
-
+    $http.get("data/vouchers.json").success(function(data) {
+      $scope.vouchers = data;
+    });
+  }]);
+  
 })();
