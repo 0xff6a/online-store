e378c8376c9ae73cbf745db3e2c94505f7bf0831
Jeremy Fox <foxjerem@gmail.com>
2014-12-03 12:00:05 +0000

    updated feature tests

--- test/feature/StoreDisplay.js
+++ test/feature/StoreDisplay.js
@@ -1,16 +1,26 @@
 describe('Online Store Display', function() {
 
+  var productList;
+
+  beforeEach(function() {
+    browser.get('app/index.html');
+    productList = element.all(by.repeater('product in products'));
+    voucherList = element.all(by.repeater('voucher in vouchers'));
+  });
+
   describe('Products', function() {
 
-    var productList;
-
-    beforeEach(function() {
-      browser.get('app/index.html');
-      productList = element.all(by.repeater('product in products'));
-    });
-
-    it('should display a list of all products', function() {
+    it('should display a list of all products and associated information', function() {
+      var name = element.all(by.repeater('product in products').column('product.name')).first().getText();
+      var category = element.all(by.repeater('product in products').column('product.category')).first().getText();
+      var price = element.all(by.repeater('product in products').column('product.price')).first().getText();
+      var stock = element.all(by.repeater('product in products').column('product.stock')).first().getText();
+    
       expect(productList.count()).toBe(13);
+      expect(name).toEqual('Almond Toe Court Shoes, Patent Black');
+      expect(category).toEqual("Women's Footwear");
+      expect(price).toEqual('£99.00');
+      expect(stock).toEqual('5');
     });
 
     it('should filter the displayed products based on user input', function() {
@@ -22,48 +32,12 @@ describe('Online Store Display', function() {
       query.clear();
       expect(productList.count()).toBe(13);
     });
+  });
 
+  describe('Vouchers', function() {
 
-    // it('should filter the phone list as a user types into the search box', function() {
-
-    //   var phoneList = element.all(by.repeater('phone in phones'));
-    //   var query = element(by.model('query'));
-
-    //   expect(phoneList.count()).toBe(20);
-
-    //   query.sendKeys('nexus');
-    //   expect(phoneList.count()).toBe(1);
-
-    //   query.clear();
-    //   query.sendKeys('motorola');
-    //   expect(phoneList.count()).toBe(8);
-    // });
-
-
-    // it('should be possible to control phone order via the drop down select box', function() {
-
-    //   var phoneNameColumn = element.all(by.repeater('phone in phones').column('phone.name'));
-    //   var query = element(by.model('query'));
-
-    //   function getNames() {
-    //     return phoneNameColumn.map(function(elm) {
-    //       return elm.getText();
-    //     });
-    //   }
-
-    //   query.sendKeys('tablet'); //let's narrow the dataset to make the test assertions shorter
-
-    //   expect(getNames()).toEqual([
-    //     "Motorola XOOM\u2122 with Wi-Fi",
-    //     "MOTOROLA XOOM\u2122"
-    //   ]);
-
-    //   element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
-
-    //   expect(getNames()).toEqual([
-    //     "MOTOROLA XOOM\u2122",
-    //     "Motorola XOOM\u2122 with Wi-Fi"
-    //   ]);
-    // });
+    it('should display a list of available vouchers', function() {
+      expect(voucherList.count()).toBe(3);
+    });
   });
 });
